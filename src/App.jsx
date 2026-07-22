33d997b41f2cbeb6bde7d1b97654a73191f4c6f1
Saul <saul.ramirez.dev@gamil.com>
2026-07-22 16:58:43 -0500

    Agrego el cambio de Colores

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ return (
 <main>
 <h1>Hola SENA, Soy Saúl Ramírez</h1>
 <br />
-<h3>Soy estudiante ADSO</h3>
+<h3>Soy estudiante ADSO, Ficha 3412768</h3>
 <br />
-<p>Esta es mi primera clase de React</p>
+<p>Esta es mi primera clase de React, y espero poder mejorar mi manera de hacer los visuales de la pagina</p>
 </main>
 )}
