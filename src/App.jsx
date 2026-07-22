52169c9af02ec811b77e814236b71f25b672c5e7
Saul <saul.ramirez.dev@gamil.com>
2026-07-22 16:29:10 -0500

    Mi primer proyecto con React

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+const fecha = new Date().toLocaleString();
+return (
+<main>
+<h1>Hola SENA, Soy Saúl Ramírez</h1>
+<br />
+<h3>Soy estudiante ADSO</h3>
+<br />
+<p>Esta es mi primera clase de React</p>
+</main>
+)}
