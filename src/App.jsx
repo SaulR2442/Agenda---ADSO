515f633f9a92a6e5223b80c02d498e51ca0c7a22
Saul <saul.ramirez.dev@gmail.com>
2026-08-05 15:44:58 -0500

    Clase 3 – Componentes y Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,66 @@
+import ContactoCard from "./components/ContactoCard";
+import "./App.css"
+
 export default function App() {
-const fecha = new Date().toLocaleString();
-return (
-<main>
-<h1>Hola SENA, Soy Saúl Ramírez</h1>
-<br />
-<h3>Soy estudiante ADSO, Ficha 3412768</h3>
-<br />
-<p>Esta es mi primera clase de React, y espero poder mejorar mi manera de hacer los visuales de la pagina</p>
-</main>
-)}
+  // Esta es nuestra "base de datos" inicial quemada en el código
+  const contactos = [
+    {
+      id: 1,
+      nombre: "Carolina Pérez",
+      telefono: "300 123 4567",
+      correo: "carolina@sena.edu.co",
+      etiqueta: "Compañera",
+    },
+    {
+      id: 2,
+      nombre: "Juan Díaz",
+      telefono: "301 987 6543",
+      correo: "juan@sena.edu.co",
+      etiqueta: "Instructor",
+    },
+    {
+      id: 3,
+      nombre: "Luisa Martínez",
+      telefono: "320 555 7788",
+      correo: "luisa@sena.edu.co",
+      etiqueta: "Cliente",
+    },
+    {
+      id: 4,
+      nombre: "Sergio Osorio",
+      telefono: "321 555 3333",
+      correo: "Sergio@sena.edu.co",
+      etiqueta: "Compañero"
+    },
+    {
+      id: 5,
+      nombre: "Adriana Alcazar",
+      telefono: "333 555 7777",
+      correo: "adriana@sena.edu.co",
+      etiqueta: "clienta"
+    },
+  ];
+
+  return (
+    <main className="app-container">
+      <h1 className="app-title">Agenda ADSO 📒</h1>
+
+      <p className="app-subtitle">Contactos guardados</p>
+
+      {/* Recorremos el arreglo contactos y pintamos una tarjeta por cada uno */}
+      {contactos.map((c) => (
+        <ContactoCard
+          key={c.id}            // key única para React
+          nombre={c.nombre}     // prop nombre
+          telefono={c.telefono} // prop telefono
+          correo={c.correo}     // prop correo
+          etiqueta={c.etiqueta} // prop etiqueta (Cliente, Instructor, etc.)
+        />
+      ))}
+
+      <p className="app-nota">
+        (Versión 0.1 - solo lectura, sin agregar ni editar todavía)
+      </p>
+    </main>
+  );
+}
